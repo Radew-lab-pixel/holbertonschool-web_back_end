11f9a924b00243a31fdc9db3deadc8071239c8ea
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-05 00:52:02 +1100

    update

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,23 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  
+  /*   const budget = {};
+
+  budget[`income-${getCurrentYear()}`] = income;
+  budget[`gdp-${getCurrentYear()}`] = gdp;
+  budget[`capita-${getCurrentYear()}`] = capita;
+    */
+
+  const year = getCurrentYear();
+  
+  const budget = {
+    [`income-${year}`]: income,
+    [`gdp-${year}`]: gdp,
+    [`capita-${year}`]: capita,
+  };
+  return budget;
+}
