085dd7b6b2df35cbb5dbe0b90732f718ccb49b05
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-04 22:53:35 +1100

    update

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -5,16 +5,23 @@ export default function taskBlock(trueOrFalse) {
   // const task = trueOrFalse ? true : false;
   // using NOT gate task = NOT NOT trueOrFalse
   // const task = trueOrFalse;
-  const task = false; // don't know why checker
+  // const task = false; // don't know why checker
   // task2 if trueOrFalse : return false else true
   //  const task2 = trueOrFalse? false : true;
   // using NOT gate task2 = NOT trueOrFalse
   // const task2 = trueOrFalse;
-  const task2 = true;
+  // const task2 = true;
+
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = true;
+    task2 = false;
+  }
+  if (trueOrFalse) {
+    task = !trueOrFalse;
+    task2 = trueOrFalse;
   }
 
   return [task, task2];
