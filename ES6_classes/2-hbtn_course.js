6853c5995e68116bf44b74c6a2f234d3039eed6d
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-06 00:41:08 +1100

    update

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,50 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    // Type checks for constructor arguments
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    // if (!Array.isArray(students) && !students.map(student=>typeof student === 'string')){
+    // every() is better than map() as it stop check after first failure while map()
+    // still process the rest
+    // every() return true only if all elements satisfied the citeria while map() doesn't as
+    // return an array
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getters
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // Setters
+  set name(value) {
+    this._name = value;
+  }
+
+  set length(value) {
+    this._length = value;
+  }
+
+  set students(value) {
+    this._students = value;
+  }
+}
