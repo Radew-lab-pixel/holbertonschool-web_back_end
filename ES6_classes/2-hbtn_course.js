38d89ca5931930faee4951e9e39329d567ee1e59
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-06 00:52:40 +1100

    update

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,22 +1,5 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    // Type checks for constructor arguments
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-
-    // if (!Array.isArray(students) && !students.map(student=>typeof student === 'string')){
-    // every() is better than map() as it stop check after first failure while map()
-    // still process the rest
-    // every() return true only if all elements satisfied the citeria while map() doesn't as
-    // return an array
-    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
-
     this._name = name;
     this._length = length;
     this._students = students;
@@ -37,14 +20,30 @@ export default class HolbertonCourse {
 
   // Setters
   set name(value) {
+    // Type checks for constructor arguments
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = value;
   }
 
   set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = value;
   }
 
   set students(value) {
+    // if (!Array.isArray(students) && !students.map(student=>typeof student === 'string')){
+    // every() is better than map() as it stop check after first failure while map()
+    // still process the rest
+    // every() return true only if all elements satisfied the citeria while map() doesn't as
+    // return an array
+    if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
     this._students = value;
   }
 }
