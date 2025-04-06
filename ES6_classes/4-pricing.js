43ebae04ccd2c8f7c547878f0d9ab5524e04cc1e
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-06 21:12:22 +1000

    update

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -34,7 +34,7 @@ export default class Pricing {
 
   // method
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} ${this._currency.code}`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   // static method
