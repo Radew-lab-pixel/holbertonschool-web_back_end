9ead59d76378361f85ff5164a5800572aa740e1a
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-12 02:12:11 +1000

    update

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,18 @@
-export default function updateUniqueItems(maps) {
+export default function updateUniqueItems(map) {
   // for map in maps
-  for (const [key, value] in maps) {
-    if (value === 1) {
+  // for (const [key, value] in maps) {
+/* for (const [key, value] of maps()) {
+  if (value === 1) {
       // value = 100;
       maps.set(key, 100);
     }
+  } */
+
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
   }
+
+  return map;
 }
