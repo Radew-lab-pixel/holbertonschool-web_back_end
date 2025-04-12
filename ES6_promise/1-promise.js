3de2ae46df408dfc4a4e2eafcd9f1efccf6b13e1
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-12 19:18:10 +1000

    update

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,8 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      //const response = { data: 'Success', status: 200 };
-      const response = { status : 200, data: "Success"};
+      // const response = { data: 'Success', status: 200 };
+      // const response = { status : 200, data: "Success"};
+      const response = { status: 200, body: 'Success' };
       resolve(response);
     } else {
       reject(new Error('The fake API is not working currently'));
