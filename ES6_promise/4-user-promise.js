cdb60024bdeba3689b2db9b7d2e10740851e206b
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-13 01:43:15 +1000

    update

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,8 +1,9 @@
 export default function signUpUser(firstName, lastName) {
-  /*   return Promise.resolve(
-    `{firstName: ${firstName}, lastName: ${lastName} }`,
+  return Promise.resolve(
+    // `{firstName: ${firstName}, lastName: ${lastName} }`,
+    { firstName, lastName },
     // {"firstName": "Guillaume", "lastName": "Salva"}
-  ); */
+  );
 
-  return Promise.resolve({ firstName, lastName });
+  // return Promise.resolve({ firstName, lastName });
 }
