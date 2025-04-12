049c93c109619916afc89ac6a190f4d90e689d3b
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-12 21:27:35 +1000

    update

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,22 +1,26 @@
 export default function handleResponseFromAPI(promise) {
   // return
   // promise.then((response) => {
-  promise
+  return promise
 
   // .then((response) => {
-    // console.log(`status : ${response.status}, body : ${response.body}`);
-    // const response = { status: 200, body: 'success' };
-    // console.log(`status: ${response.status}, body: ${response.body}`);
+  // console.log(`status : ${response.status}, body : ${response.body}`);
+  // const response = { status: 200, body: 'success' };
+  // console.log(`status: ${response.status}, body: ${response.body}`);
   //  console.log(response);
-    // return response;
-    .then(() => {
+  // return response;
+  /* .then(() => {
       const response = { status: 200, body: 'success' };
       // console.log(`status: ${response.status}, body: ${response.body}`);
       return (`status: ${response.status}, body: ${response.body}`);
-    })
+    }) */
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
     .catch((error) => {
       console.error(error);
-      return (error);
+      // return (error);
     })
     .finally(() => { // alway execute regardless
       console.log('Got a response from the AP');
