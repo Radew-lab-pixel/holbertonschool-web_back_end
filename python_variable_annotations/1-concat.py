68dff41e05ad2397776935d0e3c45e57e46af915
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-22 11:23:11 +1000

    update :

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,13 +1,14 @@
-#!/usr/bin/python3
-
-"""concat
-
-Keyword arguments:
-argument -- str
-Return: return_description
-"""
+#!/usr/bin/env python3
+""" #!/usr/bin/python3 """
 
 
 def concat(str1: str, str2: str) -> str:
+
+    """concat
+
+    Keyword arguments:
+    argument -- str
+    Return: return_description
+    """
 
     return str1 + str2
