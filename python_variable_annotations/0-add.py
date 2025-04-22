8c32247de1bad7cd326474bb75a5c25960ff0404
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-22 11:13:20 +1000

    update :

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def add(a:float, b:float) -> float:
+def add(a: float, b: float) -> float:
+    """add function
+
+    Keyword arguments:
+    argument -- a , b (float)
+    Return: a + b
+    """
+
     return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""concat
+
+Keyword arguments:
+argument -- str
+Return: return_description
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+
+    return str1 + str2
