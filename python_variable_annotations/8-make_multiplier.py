998c2a6c475ffa6dbcf1f59b7e46418c20f0ee0d
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-26 01:22:08 +1000

    update

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -3,6 +3,13 @@ from typing import Callable
 
 
 def make_multiplier(multipler: float) -> Callable[[float], float]:
+    """ function make_multipler
+    
+    arguments:
+    multipler -- float
+    Return : multipler * value
+    """
+    
 
     def multiple_func(value: float) -> float:
         return value * multipler
