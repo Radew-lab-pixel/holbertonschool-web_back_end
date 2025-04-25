d2e4ae966433d7f11c9a6beb1cdd1ff26a92d9cf
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-25 22:36:11 +1000

    update

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calculate the sum of a list of floats.
+
+    Args:
+        input_list: A list containing float numbers
+
+    Returns:
+        The sum of all numbers in the list as a float
+    """
+    return sum(input_list)
+
+
+# def sum_list(input_list: list[float]) -> float:
