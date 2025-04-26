e564663c4520a0152daf7b040381c37a6c6d608a
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-26 16:08:22 +1000

    update

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,12 +4,16 @@ you’ve written and write an async routine called wait_n
 that takes in 2 int arguments (in this order): n and
 max_delay
 """
+from typing import List
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 # from 0-basic_async_syntax import wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:  # needed for checker
+    # async def wait_n(n: int, max_delay: int) -> list:
+    # passed in terminal failed checker
+
     """wait_n
     arguments:
         n : number of loops
