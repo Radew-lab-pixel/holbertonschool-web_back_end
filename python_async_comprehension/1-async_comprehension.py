a412ef0822e6677f9198a0a9891026903608ebc7
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-29 14:27:27 +1000

    update

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    # return [num async for num]
+    numbers = []
+    count = 0
+    async for num in async_generator():
+        random_float = random.random()
+        numbers.append(random_float)
+        # count += 1
+        # if count >= 10:
+        #    break
+    return numbers
