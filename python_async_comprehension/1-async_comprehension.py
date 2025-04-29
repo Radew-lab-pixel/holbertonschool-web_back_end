68d298ed63d3b075fafddd76d8c944a8a82ce4dc
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-29 14:33:01 +1000

    update

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,14 +2,15 @@
 
 import asyncio
 import random
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     # return [num async for num]
     numbers = []
-    count = 0
+    # count = 0
     async for num in async_generator():
         random_float = random.random()
         numbers.append(random_float)
