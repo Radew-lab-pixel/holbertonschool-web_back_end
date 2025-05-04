8deadf091130b7a60974b99c3ad4ef2818817e3d
root <ktmqliu@gmail.com>
2025-05-04 13:44:37 +0000

    update

---
+++ NoSQL/students.js
@@ -0,0 +1,60 @@
+student = {
+  "firstName": "Retha",
+  "lastName": "Killeen",
+  "email": "rkilleen@mysql.com",
+  "gender": "F",
+  "country": "Phillippines",
+  "isStudentActive": false,
+  "favouriteSubjects": [
+    "maths",
+    "english",
+    "it"
+  ],
+  "totalSpentInBooks": 0.00
+}
+
+
+students = [
+  {
+  "firstName": "Retha",
+  "lastName": "Killeen",
+  "email": "rkilleen@mysql.com",
+  "gender": "F",
+  "country": "Phillippines",
+  "isStudentActive": false,
+  "favouriteSubjects": [
+    "maths",
+    "english",
+    "it"
+  ],
+  "totalSpentInBooks": 0.00
+},
+
+{
+    "firstName": "Meow",
+  "lastName": "Loeen",
+  "email": "meow@mysql.com",
+  "gender": "M",
+  "country": "Singapore",
+  "isStudentActive": false,
+  "favouriteSubjects": [
+    "maths",
+    "english",
+    "science"
+  ],
+  "totalSpentInBooks": 0.00
+  },
+  {
+    "firstName": "Kim",
+  "lastName": "Convience",
+  "email": "kim@mysql.com",
+  "gender": "M",
+  "country": "Korea",
+  "isStudentActive": true,
+  "favouriteSubjects": [
+    "maths",
+    "korean",
+    "food"
+  ],
+  "totalSpentInBooks": 0.00
+  }]
