1087fb3921c6453ef38a9a5301a02d9e4985a12f
root <ktmqliu@gmail.com>
2025-05-05 16:35:53 +0000

    update

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""function that inserts a new document
+in a collection based on kwargs:"""
+
+
 def insert_school(mongo_collection, **kwargs):
     """
     function insert_school
@@ -7,5 +12,5 @@ def insert_school(mongo_collection, **kwargs):
     Return : id of new document
     """
     new_doc = mongo_collection.insert_one(kwargs)
-    # return 
+    # return
     return new_doc.inserted_id
