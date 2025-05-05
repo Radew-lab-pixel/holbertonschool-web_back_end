40d47411609e1916bd146e61c91dd2984b5a06d7
root <ktmqliu@gmail.com>
2025-05-05 00:55:49 +0000

    update

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-""" 8-main """
+"""  Write a Python function that lists all documents in a collection
+"""
 from typing import Dict, List
 from pymongo import MongoClient
-import pprint
+# import pprint
 
 
 def list_all(mongo_collection) -> List[Dict]:  # not list but List
+    """ list_all
+      argument :
+      mongo_collection : mongo collections object
+      Return : list of documents in collection
+    """
     #  for doc in tutorial.find():
     #   pprint.pprint(doc)
     find_result = mongo_collection.find({})
