9bb9f982aec51bc5229e205c31f08a9ed15f2c45
root <ktmqliu@gmail.com>
2025-05-06 13:54:02 +0000

    update

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env_python3
+"""Python script that provides some stats
+about Nginx logs stored in MongoDB:"""
+from pymongo import MongoClient
+
+
+client = MongoClient("mongodb://localhost:27017/")
+
+# db = client["logs"]  # create or use database "logs"
+# collection = db["nginx"]  # create or use collection "nginx"
+
+# collection = client.logs.nginx  # new way after Mongo 4
+db = client.logs
+collection = db.nginx
+n_logs = collection.count_documents({})
+print(f"{n_logs} logs")
+print("Methods:")
+n_Get = collection.count_documents({"method": "GET"})
+print(f"\tmethod GET: {n_Get}")
+n_Post = collection.count_documents({"method": "POST"})
+print(f"\tmethod POST: {n_Post}")
+n_Put = collection.count_documents({"method": "PUT"})
+print(f"\tmethod PUT: {n_Put}")
+n_Patch = collection.count_documents({"method": "PATCH"})
+print(f"\tmethod PATCH: {n_Patch}")
+n_Delete = collection.count_documents({"method": "DELETE"})
+print(f"\tmethod DELETE: {n_Delete}")
+n_status = collection.count_documents({"path": "/status"})
+print(f"{n_status} status check")
