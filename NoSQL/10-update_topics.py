e35df58957b3fccb795b02f8f1edfdaaf1ec9bb6
root <ktmqliu@gmail.com>
2025-05-06 00:01:49 +0000

    update

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""function that changes all topics of a
+school document based on the name:"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """ up_date_topics
+    Arguments:
+    mongo_collection : mongoDB collection object
+    name : name of the document
+    topics : value of docuument
+    Return none
+    """
+    new_update = mongo_collection.update_one({"name": name},
+                                             {"$set": {"topics": topics}})
