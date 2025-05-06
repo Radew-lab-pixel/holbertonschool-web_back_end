9a5b507fd8db2d87266a47771bc5b78e90a5a3c5
root <ktmqliu@gmail.com>
2025-05-06 03:35:46 +0000

    backup

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -13,10 +13,10 @@ def update_topics(mongo_collection, name, topics):
     """
     # new_update = mongo_collection.update_one({"name": name},
     #                                          {"$set": {"topics": topics}})
-    new_update = mongo_collection.update_many({"name": {"$gt": name}},
-                                            {"$set": {"topics": topics}})
-    # new_update = mongo_collection.update_many({"name": name},
-    #                                           {"$set": {"topics": topics}})
+    # new_update = mongo_collection.update_many({"name": {"$gt": name}},  # $gt is grester than
+    #                                         {"$set": {"topics": topics}})
+    new_update = mongo_collection.update_many({"name": name},
+                                              {"$set": {"topics": topics}})
     # print("OK")  # needed by checker
     # print (new_update)
     # print (new_update.modified_count)
